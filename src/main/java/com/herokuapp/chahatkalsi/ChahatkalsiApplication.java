5ed421210e4b48178980ec6c81b71581f186e551
Chahat Kalsi <chahat.ck88@gmail.com>
2023-01-13 01:58:30 +0530

    initial commit

---
+++ src/main/java/com/herokuapp/chahatkalsi/ChahatkalsiApplication.java
@@ -0,0 +1,13 @@
+package com.herokuapp.chahatkalsi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChahatkalsiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChahatkalsiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/herokuapp/chahatkalsi/ChahatkalsiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.herokuapp.chahatkalsi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChahatkalsiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
