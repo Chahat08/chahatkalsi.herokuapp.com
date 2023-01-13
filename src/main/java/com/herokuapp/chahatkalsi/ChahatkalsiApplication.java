1cc6f51a2aa2351fa6ae4c627c90270375d697fb
Chahat Kalsi <chahat.ck88@gmail.com>
2023-01-13 07:36:37 +0530

    initial redirect method created

--- src/main/java/com/herokuapp/chahatkalsi/ChahatkalsiApplication.java
+++ src/main/java/com/herokuapp/chahatkalsi/ChahatkalsiApplication.java
@@ -1,10 +1,22 @@
 package com.herokuapp.chahatkalsi;
 
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.web.bind.annotation.*;
+
+@RestController
 @SpringBootApplication
 public class ChahatkalsiApplication {
+
+	private static String projectUrl = "https://en.wikipedia.org/wiki/Burkhan_Khaldun";
+
+	@RequestMapping(value = "/redirect", method = RequestMethod.GET)
+	public void method(HttpServletResponse httpServletResponse) {
+		httpServletResponse.setHeader("Location", this.projectUrl);
+		httpServletResponse.setStatus(302);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ChahatkalsiApplication.class, args);
